c6d5d532df3db09d690172ba5b940600f8c56f68
Patrick McKenzie <pfm2119@columbia.edu>
2020-09-04 17:42:21 +0000

    modified bash script for timeout, removed some failed vids

--- scripts/download_upload_clip.sh
+++ scripts/download_upload_clip.sh
@@ -2,7 +2,7 @@
 
 NOW=$(date +"%Y_%m_%d_%I_%M_%p")
 
-ffmpeg -i $(youtube-dl -f 96 -g https://www.youtube.com/watch?v=4R8F4XZd9Us) -t 00:01:00.00 -c copy /pinky/patrick/birdclips/clips/${NOW}.mp4
+timeout 120 ffmpeg -i $(youtube-dl -f 96 -g https://www.youtube.com/watch?v=4R8F4XZd9Us) -t 00:01:00.00 -c copy /pinky/patrick/birdclips/clips/${NOW}.mp4
 
 git add /pinky/patrick/birdclips/clips/${NOW}.mp4
 git commit -m "added clip ${NOW}.mp4"
