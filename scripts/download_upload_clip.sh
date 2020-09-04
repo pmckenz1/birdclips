5ef3f6de047562d1a778174c2a8464abf150cf8a
Patrick McKenzie <pfm2119@columbia.edu>
2020-09-04 17:46:05 +0000

    downgraded quality for download, hoping it will be more successful!

--- scripts/download_upload_clip.sh
+++ scripts/download_upload_clip.sh
@@ -2,7 +2,7 @@
 
 NOW=$(date +"%Y_%m_%d_%I_%M_%p")
 
-timeout 120 ffmpeg -i $(youtube-dl -f 96 -g https://www.youtube.com/watch?v=4R8F4XZd9Us) -t 00:01:00.00 -c copy /pinky/patrick/birdclips/clips/${NOW}.mp4
+timeout 300 ffmpeg -i $(youtube-dl -f 95 -g https://www.youtube.com/watch?v=4R8F4XZd9Us) -t 00:01:00.00 -c copy /pinky/patrick/birdclips/clips/${NOW}.mp4
 
 git add /pinky/patrick/birdclips/clips/${NOW}.mp4
 git commit -m "added clip ${NOW}.mp4"
