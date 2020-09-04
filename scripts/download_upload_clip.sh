7b484e80f06d57a2f7ab5e7431efe4dd5f731520
Patrick McKenzie <pfm2119@columbia.edu>
2020-09-04 03:15:33 +0000

    added scripts and removed test clips

---
+++ scripts/download_upload_clip.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+NOW=$(date +"%Y_%m_%d_%I_%M_%p")
+
+ffmpeg -i $(youtube-dl -f 96 -g https://www.youtube.com/watch?v=4R8F4XZd9Us) -t 00:00:30.00 -c copy /pinky/patrick/birdclips/clips/${NOW}.mp4
+
+git add /pinky/patrick/birdclips/clips/${NOW}.mp4
+git commit -m "added clip ${NOW}.mp4"
+git push
+
+rm /pinky/patrick/birdclips/clips/${NOW}.mp4
--- scripts/run_test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-NOW=$(date +"%Y_%m_%d_%I_%M_%p")
-
-ffmpeg -i $(youtube-dl -f 96 -g https://www.youtube.com/watch?v=4R8F4XZd9Us) -t 00:00:30.00 -c copy /pinky/patrick/bird_stream/clips/${NOW}.mp4
-
